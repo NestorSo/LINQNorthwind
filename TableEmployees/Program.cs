b26e04f465d3a5fd309560f08e52b85e737d9188
NestorSo <Nestorsoto2601@gmail.com>
2024-03-19 20:48:17 -0600

    Agregar archivos de proyecto.

---
+++ TableEmployees/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using TableEmployees.Data;
+using TableEmployees.Models;
+
+List<Employee> Employees = new List<Employee>(); //Supongamos que esta función devuelve una lista de objetos de la clase Persona.
+
+
+
+using (var context = new NorthwindContext())
+{
+
+    var personasEnMadrid = Employees.Where(p => p.Ciudad == "Madrid");
+    foreach (var Employee in EmployeeTitle)
+    {
+        Console.WriteLine($"Nombre: {persona.Nombre}, Edad: {persona.Edad}, Ciudad: {persona.Ciudad}");
+    }
+}
